f560c4e4350eff9f1141b8a356bd720667ba90ea
Andrew Blackburn <andyblac@icloud.com>
2019-09-26 15:10:06 +0100

    code cleanup, with fix for login issue due to Web 5.2, and fix for Ongoing Alarm not being detected in HomeKit.

--- risco.js
+++ risco.js
@@ -17,45 +17,38 @@ function init(aUser, aPassword, aPIN, aSiteId, context) {
     risco_siteId = aSiteId;
     self = context;
     req_counter = 0;
-
 }
+
 
 function extractError(aBody) {
     var serverInfo_begin = aBody.indexOf("<span class=\"infoServer\">");
     var serverInfo_end = aBody.indexOf("</span>", serverInfo_begin);
     return aBody.substring(serverInfo_begin + 26, serverInfo_end - 7);
-
 }
+
 
 function login() {
     return new Promise(function (resolve, reject) {
-        //self.log('login [step1] to RiscoCloud first stage...');
-
-        var post_data = {
-            "username": risco_username,
-            "password": risco_password,
-            "strRedirectToEventUID": "",
-            "langId": "en"
+        // self.log('login [step1] to RiscoCloud first stage...');
+        var post_data = 'username=' + encodeURIComponent(risco_username) + '&password=' + encodeURIComponent(risco_password);
+        var header_data = {
+            'Content-Length': post_data.length,
+            'Content-type': 'application/x-www-form-urlencoded'
         };
 
         var options = {
             url: 'https://www.riscocloud.com/ELAS/WebUI/',
             method: 'POST',
-            headers: {},
-            json: post_data
+            headers: header_data,
+            body: post_data
         };
 
         request(options, function (err, res, body) {
             try {
                 if (!err && res.statusCode == 302) {
-                    //self.log('Got Cookie, save it');
+                    // self.log('Got Cookie, save it');
                     riscoCookies = res.headers['set-cookie'];
-
-                    var post_data = {
-                        "SelectedSiteId": risco_siteId,
-                        "Pin": risco_pincode
-                    };
-
+                    var post_data = 'SelectedSiteId=' + risco_siteId + '&Pin='+ risco_pincode;
                     var options = {
                         url: 'https://www.riscocloud.com/ELAS/WebUI/SiteLogin',
                         method: 'POST',
@@ -63,30 +56,30 @@ function login() {
                             'Cookie': riscoCookies,
                             'Host': 'www.riscocloud.com',
                             'Origin': 'https://www.riscocloud.com',
-                            'Referer': 'https://www.riscocloud.com/ELAS/WebUI/SiteLogin/Index'
+                            'Referer': 'https://www.riscocloud.com/ELAS/WebUI/SiteLogin/Index',
+                            'Content-Length': post_data.length,
+                            'Content-type': 'application/x-www-form-urlencoded'
                         },
-                        json: post_data
+                        body: post_data
                     };
                     request(options, function (err, res, body) {
                         try {
                             if (!err && res.statusCode == 302) {
-                                //self.log('LoggedIn !');
+                                // self.log('LoggedIn !');
                                 resolve();
                                 return
                             } else {
                                 self.log('Status Code: ', res.statusCode);
-                                //self.log('login [step2] > error:', extractError(body));
+                                self.log('login [step2] > error:', extractError(body));
                                 reject('');
                                 return
                             }
-
                         } catch (error) {
                             self.log(error);
                             reject('');
                             return
                         }
                     })
-
                 } else {
                     self.log('Status Code: ', res.statusCode);
                     self.log('login [step1] > error:', extractError(body));
@@ -104,10 +97,8 @@ function login() {
 
 
 function getState() {
-
     return new Promise(function (resolve, reject) {
         var post_data = {};
-
         var options = {
             url: 'https://www.riscocloud.com/ELAS/WebUI/Overview/Get',
             method: 'POST',
@@ -135,35 +126,30 @@ function getState() {
                     return
                 }
 
-                //self.log('RiscoCloud ArmedState:' + body.overview.partInfo.armedStr + " / RiscoCloud OngoingAlarm: " + body.OngoingAlarm );
-                var riscoState;
+                // self.log('RiscoCloud ArmedState: ' + body.overview.partInfo.armedStr + ' / PartArmedState: ' + body.overview.partInfo.partarmedStr);
                 // 0 -  Characteristic.SecuritySystemTargetState.STAY_ARM:
                 // 1 -  Characteristic.SecuritySystemTargetState.AWAY_ARM:
                 // 2-   Characteristic.SecuritySystemTargetState.NIGHT_ARM:
                 // 3 -  Characteristic.SecuritySystemTargetState.DISARM:
                 //self.log(body);
 
-                if (body.OngoingAlarm == true) {
-                    riscoState = 4;
-                } else {
-                    try {
-                        var armedZones = body.overview.partInfo.armedStr.split(' ');
-                        var partArmedZones = body.overview.partInfo.partarmedStr.split(' ');
+                var riscoState;
+                try {
+                    var armedZones = body.overview.partInfo.armedStr.split(' ');
+                    var partArmedZones = body.overview.partInfo.partarmedStr.split(' ');
 
-                        if (parseInt(armedZones[0]) > 0) {
-                            riscoState = 1; // Armed
-                        } else if (parseInt(partArmedZones[0]) > 0) {
-                            riscoState = 2; // Partially Armed
-                        } else {
-                            riscoState = 3; // Disarmed
-                        }
-                    } catch (error) {
-                        self.log(error);
-                        reject();
-                        return
+                    if (parseInt(armedZones[0]) > 0) {
+                        riscoState = 1; // Armed
+                    } else if (parseInt(partArmedZones[0]) > 0) {
+                        riscoState = 2; // Partially Armed
+                    } else {
+                        riscoState = 3; // Disarmed
                     }
+                } catch (error) {
+                    self.log(error);
+                    reject();
+                    return
                 }
-
                 resolve(riscoState);
             } else {
                 self.log(err);
@@ -174,9 +160,10 @@ function getState() {
     })
 }
 
-function refreshState() {
 
+function getCPState() {
     return new Promise(function (resolve, reject) {
+        // self.log('risco.getCPState');
         var alive_url
 
         if (req_counter == 0) {
@@ -191,7 +178,6 @@ function refreshState() {
         }
 
         var post_data = {};
-
         var options = {
             url: alive_url,
             method: 'POST',
@@ -208,6 +194,7 @@ function refreshState() {
                 // Check error inside JSON
                 try {
                     if (body.error == 3) {
+                        //self.log('Error: 3. Try to login first.');
                         reject();
                         return
                     }
@@ -216,111 +203,16 @@ function refreshState() {
                     reject();
                     return
                 }
-
-                var riscoState;
-                // 0 -  Characteristic.SecuritySystemTargetState.STAY_ARM:
-                // 1 -  Characteristic.SecuritySystemTargetState.AWAY_ARM:
-                // 2-   Characteristic.SecuritySystemTargetState.NIGHT_ARM:
-                // 3 -  Characteristic.SecuritySystemTargetState.DISARM:
-                //self.log(body);
-
+                // self.log(body);
                 if (body.OngoingAlarm == true) {
-                    riscoState = 4;
+                    self.log("RiscoCloud OngoingAlarm: " + body.OngoingAlarm );
+                    resolve(4);
+                    return
                 } else {
                     // Try different GET Method
-
-                    if (body.overview == undefined) {
-                        //self.log('No changes');
-                        resolve();
-                        return
-                    }
-
-                    try {
-                        var armedZones = body.overview.partInfo.armedStr.split(' ');
-                        var partArmedZones = body.overview.partInfo.partarmedStr.split(' ');
-
-                        //self.log('armedZones:', armedZones[0]);
-
-                        if (parseInt(armedZones[0]) > 0) {
-                            riscoState = 1; // Armed
-                        } else if (parseInt(partArmedZones[0]) > 0) {
-                            riscoState = 2; // Partially Armed
-                        } else {
-                            riscoState = 3; // Disarmed
-                        }
-
-                        resolve(riscoState);
-                        return
-
-
-                    } catch (error) {
-                        self.log('Failed during parse arm / partArmed zones', error);
-                        reject();
-                        return
-                    }
-
-                    /*
-                    var post_data = {};
-
-                    var options = {
-                        url: 'https://www.riscocloud.com/ELAS/WebUI/Overview/Get',
-                        method: 'POST',
-                        headers: {
-                            "Referer": "https://www.riscocloud.com/ELAS/WebUI/MainPage/MainPage",
-                            "Origin": "https://www.riscocloud.com",
-                            "Cookie": riscoCookies
-                        },
-                        json: post_data
-                    };
-                    
-                    request(options, function (err, res, body) {
-                        if (!err) {
-                            // Check error inside JSON
-                            try {
-                                if (body.error == 3) {
-                                    reject();
-                                    return
-                                }
-                            } catch (error) {
-                                self.log('Failed during GET GetCPState');
-                                reject();
-                                return
-                            }
-                            
-                            try {
-                                var armedZones = body.overview.partInfo.armedStr.split(' ');
-                                var partArmedZones = body.overview.partInfo.partarmedStr.split(' ');
-
-                                self.log('armedZones:', armedZones[0]);
-                                self.log('partarmedZones:', partArmedZones[0]);
-                                if (parseInt(armedZones[0]) > 0) {
-                                    riscoState = 1; // Armed
-                                } else if (parseInt(partArmedZones[0]) > 0) {
-                                    riscoState = 2; // Partially Armed
-                                } else {
-                                    riscoState = 3; // Disarmed
-                                }
-
-                                resolve(riscoState);
-                                return
-
-
-                            } catch (error) {
-                                self.log('Failed during parse arm / partArmed zones', error);
-                                reject();
-                                return
-                            }
-
-                        } else {
-                            self.log('Error during request /WebUI/Overview/Get')
-                            reject();
-                            return
-                        }
-                       
-
-                    });
-                    */
-
+                    // self.log('Not Ongoing Alarm');
+                    resolve('Not Ongoing Alarm');
+                    return
                 }
             } else {
                 self.log('Error during GetCPState');
@@ -331,12 +223,9 @@ function refreshState() {
 }
 
 
-
-
 function arm(aState, cmd) {
-
-    //console.log('func: arm');
     return new Promise(function (resolve, reject) {
+        //console.log('func: arm');
 
         var targetType = cmd;
         var targetPasscode;
@@ -349,21 +238,20 @@ function arm(aState, cmd) {
             targetPasscode = "------"
         }
 
-        var post_data = {
-            "type": targetType,
-            "passcode": targetPasscode,
-            "bypassZoneId": -1
+        var post_data = 'type=' + targetType + '&passcode=' + targetPasscode  + '&bypassZoneId=-1';
+        var header_data = {
+            "Referer": "https://www.riscocloud.com/ELAS/WebUI/MainPage/MainPage",
+            "Origin": "https://www.riscocloud.com",
+            "Cookie": riscoCookies,
+            'Content-Length': post_data.length,
+            'Content-type': 'application/x-www-form-urlencoded'
         };
 
         var options = {
             url: 'https://www.riscocloud.com/ELAS/WebUI/Security/ArmDisarm',
             method: 'POST',
-            headers: {
-                "Referer": "https://www.riscocloud.com/ELAS/WebUI/MainPage/MainPage",
-                "Origin": "https://www.riscocloud.com",
-                "Cookie": riscoCookies
-            },
-            json: post_data
+            headers: header_data,
+            body: post_data
         };
 
         request(options, function (err, res, body) {
@@ -391,10 +279,11 @@ function arm(aState, cmd) {
     });
 }
 
+
 module.exports = {
     init,
     login,
     getState,
-    refreshState,
+    getCPState,
     arm
 };
